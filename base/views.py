41d5872a0fe0dd0ae0cd67a877cbe301de5d09b0
NazarSenhuk <senchuknazar6@gmail.com>
2023-02-17 18:42:22 +0200

    add RDS

--- base/views.py
+++ base/views.py
@@ -5,5 +5,20 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.http import HttpResponse
 def email_send(request):
-	
-	return HttpResponse('Email was sent!')
+	if request.POST:
+		email = request.POST.get('email')
+		name =   request.POST.get('name')
+		message =  request.POST.get('message')
+		template = render_to_string('email.html', {
+			'name' :name,
+			'email': email,
+			'message': message
+
+
+
+
+			})
+		message =EmailMessage('portfolio',template,settings.EMAIL_HOST_USER,['senchuknazar6@gmail.com'])
+		message.fail_silently = False
+		message.send()
+		return HttpResponse('Email was sent!')
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,8 +79,14 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'demo_1',
+        'USER':'senchuknazardb',
+        'PASSWORD': 'senchuknazar6',
+        'HOST' : "database-2.cvovowomxjfc.us-east-1.rds.amazonaws.com",
+        'POST': '5432'
+
+
     }
 }
 
